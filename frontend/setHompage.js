51d138f008ea0c26c7bb8f7afc837b3c29d29f33
tarr4h <81558152+tarr4h@users.noreply.github.com>
2024-12-10 14:26:45 +0000

    Deploying to adp from @ tarr4h/T_WEB@101f1f417a192064a11b10f34acebcec1d97beb9 🚀

--- frontend/setHompage.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-const { REACT_APP_HOMEPAGE_URL } = process.env
-
-const data = JSON.parse(fs.readFileSync('package.json', 'utf-8'))
-data.homepage = REACT_APP_HOMEPAGE_URL
-fs.writeFileSync('package.json', JSON.stringify(data, null, 2))
